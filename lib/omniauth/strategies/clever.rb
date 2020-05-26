9257d89c967f7f371666e0481df6966636794d4d
ChenShmil <59408446+chen-shmilovich-cm@users.noreply.github.com>
2020-05-26 12:30:54 +0300

    Update clever.rb

--- lib/omniauth/strategies/clever.rb
+++ lib/omniauth/strategies/clever.rb
@@ -4,30 +4,43 @@ require 'base64'
 module OmniAuth
   module Strategies
     class Clever < OmniAuth::Strategies::OAuth2
-      option :name, "clever"
+      # Clever is a unique OAuth 2.0 service provider in that login sequences
+      # are often initiated by Clever, not the client. When Clever initiates
+      # login, a state parameter is not relevant nor sent.
 
+      option :name, "clever"
       option :client_options, {
         :site          => 'https://api.clever.com',
         :authorize_url => 'https://clever.com/oauth/authorize',
         :token_url     => 'https://clever.com/oauth/tokens'
       }
 
-      def authorize_params
+      # This option bubbles up to the OmniAuth::Strategies::OAuth2
+      # when we call super in the callback_phase below.
+      # **State will still be verified** when login is initiated by the client.
+      option :provider_ignores_state, true
+
+      def token_params
         super.tap do |params|
-          if options.client_options.dev
-            params[:dev] = options.client_options.dev
+          params[:headers] = {'Authorization' => "Basic #{Base64.strict_encode64("#{options.client_id}:#{options.client_secret}")}"}
+        end
+      end
+
+      def callback_phase
+        error = request.params["error_reason"] || request.params["error"]
+        stored_state = session.delete("omniauth.state")
+        if error
+          fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+        else
+          # Only verify state if we've initiated login and have stored a state
+          # to compare to.
+          if stored_state && (!request.params["state"] || request.params["state"] != stored_state)
+            fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+          else
+            super
           end
         end
       end
-
-      def token_params
-        username_password = options.client_secret + ":"
-        super.tap do |params|
-          params[:headers] = {'Authorization' => "Basic #{Base64.encode64(username_password)}"}
-        end
-      end
-
-
 
       uid{ raw_info['data']['id'] }
 
@@ -43,6 +56,11 @@ module OmniAuth
 
       def raw_info
         @raw_info ||= access_token.get('/me').parsed
+      end
+
+      # Fix unknown redirect uri bug by NOT appending the query string to the callback url.
+      def callback_url
+        full_host + script_name + callback_path
       end
     end
   end
